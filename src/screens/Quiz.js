018d523ccce7eb9a16de0c96859cfe3222be51f8
YoshikiYamada <yoshikiyamada@usernoMacBook-puro.local>
2020-09-11 22:56:51 +0900

    quizAPIの利用 コンソール出力

--- src/screens/Quiz.js
+++ src/screens/Quiz.js
@@ -1,6 +1,43 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+import axios from "axios";
+
+const url = "https://quizapi.io/api/v1/questions";
+
+// config for axios
+const config = {
+  params: {
+    limit: 1,
+  },
+  headers: {
+    "X-Api-Key": process.env.REACT_APP_QUIZAPI_KEY,
+  },
+};
+
 
 const Quiz = () => {
+    useEffect(() => {
+        fetchQuiz();
+        return;
+      }, []);
+     
+     const fetchQuiz = () => {
+        console.log(process.env.REACT_APP_QUIZAPI_KEY)
+        // 情報を取得
+        axios
+          .get(url, config)
+          .then((res) => {
+            console.log(res);
+          })
+          .catch((e) => {
+            alert("エラーだよ。コンソールを見てね");
+            console.error(e);
+          })
+          .finally(() => {
+            console.log("done!");
+          });
+      };
+    
   return (
     <div>
 		<h2>クイズだよ！</h2>
